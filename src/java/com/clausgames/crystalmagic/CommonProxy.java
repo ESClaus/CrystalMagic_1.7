83ce9298f47aec17ae923ffc58e670036027bf4a
Thomas Hughes <weiseguy91@gmail.com>
2015-09-24 20:47:02 -0600

    GUIHandler Added (please don't change path) CommonProxy modified to register the GUIHandler

--- src/java/com/clausgames/crystalmagic/CommonProxy.java
+++ src/java/com/clausgames/crystalmagic/CommonProxy.java
@@ -3,11 +3,13 @@ package com.clausgames.crystalmagic;
 import com.clausgames.crystalmagic.block.ModBlocks;
 import com.clausgames.crystalmagic.crafting.ModCrafting;
 import com.clausgames.crystalmagic.item.ModItems;
+import com.clausgames.crystalmagic.network.CrystalMagicGuiHandler;
 import com.clausgames.crystalmagic.world.CrystalGeneration;
 
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.network.NetworkRegistry;
 import cpw.mods.fml.common.registry.GameRegistry;
 
 public class CommonProxy
@@ -22,12 +24,14 @@ public class CommonProxy
 
 	public void init(FMLInitializationEvent e)
 	{
+		// GuiHandler Registration
+		NetworkRegistry.INSTANCE.registerGuiHandler(CrystalMagic.instance, new CrystalMagicGuiHandler());
 		// WorldGen
 		GameRegistry.registerWorldGenerator(new CrystalGeneration(), 0);
 	}
 
 	public void postInit(FMLPostInitializationEvent e)
 	{
-		
+
 	}
 }
---
+++ src/java/com/clausgames/crystalmagic/network/CrystalMagicGuiHandler.java
@@ -0,0 +1,20 @@
+package com.clausgames.crystalmagic.network;
+
+import cpw.mods.fml.common.network.IGuiHandler;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.world.World;
+
+public class CrystalMagicGuiHandler implements IGuiHandler
+{
+	@Override
+	public Object getServerGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+	{
+		return null;
+	}
+
+	@Override
+	public Object getClientGuiElement(int ID, EntityPlayer player, World world, int x, int y, int z)
+	{
+		return null;
+	}
+}
