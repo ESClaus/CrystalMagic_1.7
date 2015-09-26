446e72a7c5f85fb71175e6c17448e083985bae4a
Claus <eric.s.clausing@gmail.com>
2015-09-26 11:29:19 -0600

    Change version to match MC Version

--- src/java/com/clausgames/crystalmagic/lib/LibMisc.java
+++ src/java/com/clausgames/crystalmagic/lib/LibMisc.java
@@ -5,7 +5,7 @@ public final class LibMisc
 	// Mod Constants
 	public static final String MODID = "cg_crystalmagic";
 	public static final String MODNAME = "Crystal Magic";
-	public static final String VERSION = "0.0.1";
+	public static final String VERSION = "1.7.10-1.0";
 	
 	// Proxy Constants
 	public static final String PROXY_COMMON = "com.clausgames.crystalmagic.CommonProxy";
