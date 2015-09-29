105cb20c29832470da67d2c801fbb145cbf5cab7
Claus <eric.s.clausing@gmail.com>
2015-09-29 10:40:47 -0600

    Crystal now renders properly in hand and on creativetab.

--- src/java/com/clausgames/crystalmagic/proxy/ClientProxy.java
+++ src/java/com/clausgames/crystalmagic/proxy/ClientProxy.java
@@ -1,9 +1,13 @@
 package com.clausgames.crystalmagic.proxy;
 
+import com.clausgames.crystalmagic.block.ModBlocks;
+import com.clausgames.crystalmagic.client.renderer.ItemRenderCrystalOre;
 import com.clausgames.crystalmagic.client.renderer.RenderCrystalOre;
 import com.clausgames.crystalmagic.tile.TileEntityBlockCrystalOre;
 import cpw.mods.fml.client.registry.ClientRegistry;
 import net.minecraft.client.renderer.tileentity.TileEntitySpecialRenderer;
+import net.minecraft.item.Item;
+import net.minecraftforge.client.MinecraftForgeClient;
 
 public class ClientProxy extends CommonProxy
 {
@@ -12,6 +16,7 @@ public class ClientProxy extends CommonProxy
 		//CrystalOreBlock
 		TileEntitySpecialRenderer render = new RenderCrystalOre();
 		ClientRegistry.bindTileEntitySpecialRenderer(TileEntityBlockCrystalOre.class, render);
+		MinecraftForgeClient.registerItemRenderer(Item.getItemFromBlock(ModBlocks.blockCrystalOre), new ItemRenderCrystalOre(render, new TileEntityBlockCrystalOre()));
 	}
 
 	public void registerTileEntitySpecialRenderer()
