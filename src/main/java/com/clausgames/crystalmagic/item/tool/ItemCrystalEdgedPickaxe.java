b8abf4d0980928a6f41d0a7a2e4705dd9097ab22
Claus <eric.s.clausing@gmail.com>
2015-10-03 16:16:10 -0600

    Working version with plants.

--- src/main/java/com/clausgames/crystalmagic/item/tool/ItemCrystalEdgedPickaxe.java
+++ src/main/java/com/clausgames/crystalmagic/item/tool/ItemCrystalEdgedPickaxe.java
@@ -1,14 +1,19 @@
 package com.clausgames.crystalmagic.item.tool;
 
+import com.clausgames.crystalmagic.achievement.ICraftAchievement;
+import com.clausgames.crystalmagic.achievement.ModAchievements;
 import com.clausgames.crystalmagic.creativetab.CreativeTabCrystalMagic;
 import com.clausgames.crystalmagic.lib.LibMisc;
 import cpw.mods.fml.relauncher.Side;
 import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.client.renderer.texture.IIconRegister;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemPickaxe;
 import net.minecraft.item.ItemStack;
+import net.minecraft.stats.Achievement;
 
-public class ItemCrystalEdgedPickaxe extends ItemPickaxe
+public class ItemCrystalEdgedPickaxe extends ItemPickaxe implements ICraftAchievement
 {
     public ItemCrystalEdgedPickaxe(String unlocalizedName, ToolMaterial material)
     {
@@ -17,5 +22,11 @@ public class ItemCrystalEdgedPickaxe extends ItemPickaxe
         this.setTextureName(LibMisc.MODID + ":" + unlocalizedName);
         this.setCreativeTab(CreativeTabCrystalMagic.tabCrystalMagic);
         this.setMaxStackSize(1);
+    }
+
+    @Override
+    public Achievement getAchievementOnCraft(ItemStack stack, EntityPlayer player, IInventory matrix)
+    {
+        return ModAchievements.crystalEdgedPickaxeCraft;
     }
 }
