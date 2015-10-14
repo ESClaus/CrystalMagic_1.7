aae7ac09bffd77e8a0b4118c43221d46407a5e65
Thomas Hughes <weiseguy91@gmail.com>
2015-10-13 19:55:21 -0600

    Refactored ItemCrystalFragment to get ready for adding CrystalCodex give command on achievement get.

--- src/main/java/com/clausgames/crystalmagic/item/ItemCrystalFragment.java
+++ src/main/java/com/clausgames/crystalmagic/item/ItemCrystalFragment.java
@@ -3,6 +3,7 @@ package com.clausgames.crystalmagic.item;
 import com.clausgames.crystalmagic.achievement.IPickupAchievement;
 import com.clausgames.crystalmagic.achievement.ModAchievements;
 import com.clausgames.crystalmagic.lib.LibMisc;
+import net.minecraft.command.CommandGive;
 import net.minecraft.entity.item.EntityItem;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
@@ -19,6 +20,13 @@ public class ItemCrystalFragment extends ItemCrystalMagic implements IPickupAchi
 
     @Override
     public Achievement getAchievementOnPickup(ItemStack stack, EntityPlayer player, EntityItem item) {
-        return stack.getItemDamage() == 0 ? ModAchievements.crystalFragmentPickup : null;
+        if (stack.getItemDamage() == 0)
+        {
+            //TODO Add code to give player CrystalCodex when this code runs.
+            return ModAchievements.crystalFragmentPickup;
+        } else
+        {
+            return null;
+        }
     }
 }
